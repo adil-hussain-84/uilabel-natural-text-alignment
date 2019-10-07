740837ee6b0f8e086825bd288ecf6e2b0ce44de1
Adil Hussain <adilson05uk@gmail.com>
2019-10-07 14:43:30 +0100

    Added simple iOS application which demonstrates the problem of Arabic text not right-aligning with UILabels.

---
+++ Incorrect Natural Text Alignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Incorrect Natural Text Alignment
+//
+//  Created by Adil Hussain on 07/10/2019.
+//  Copyright © 2019 Adil Hussain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = "\u{202E}العربية\u{202C}"
+    }
+}
